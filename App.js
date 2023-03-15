fe4476291ecaf271d9838599801e465ac804bd8d
Quinta11 <quinta11@my.erau.edu>
2023-03-15 19:55:32 -0400

    Added login and registration process, and barebones homepage navigation

--- App.js
+++ App.js
@@ -1,2 +1,14 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import LoginNavigation from './src/navigation/LoginNavigation';
+
+function App() {
+    return (
+        <NavigationContainer>
+            <LoginNavigation />
+        </NavigationContainer>
+    );
+}
 
 export default App;
